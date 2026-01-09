605eb16fadb68ea8929c0b1769ed4f4e7c055160
lreicher <luca.reicher11@gmail.com>
2026-01-09 09:21:25 +0100

    Speed Veränderung(in progress)

--- Assets/MyGame/Scripts/Snowball.cs
+++ Assets/MyGame/Scripts/Snowball.cs
@@ -6,12 +6,20 @@ public class Snowball : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        speed = 5f;
+        if (speed == 0)
+            speed = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(speed);
         transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        speed = 0f;
+        Debug.Log("Schneeball hat Trigger betreten");
     }
 }
